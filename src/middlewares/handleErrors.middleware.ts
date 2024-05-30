7ad71cd445e67bd95f2b47ebcecb9b220ad4319e
Pedro Mendonça <althander@gmail.com>
2024-05-30 10:25:43 -0300

    Primeira versão
    Atividade para praticar autenticação

--- src/middlewares/handleErrors.middleware.ts
+++ src/middlewares/handleErrors.middleware.ts
@@ -1,13 +1,23 @@
 import { NextFunction, Request, Response } from "express";
 import { AppError } from "../errors/appError";
+import { JsonWebTokenError } from "jsonwebtoken";
 
 export class HandleErrors {
-   static execute(error: Error, req: Request, res: Response, next: NextFunction) {
-      if (error instanceof AppError) {
-         return res.status(error.statusCode).json({ message: error.message });
-      }
+  static execute(
+    error: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    if (error instanceof AppError) {
+      return res.status(error.statusCode).json({ message: error.message });
+    }
 
-      console.log(error);
-      return res.status(500).json({ message: "Internal server error" });
-   }
+    if (error instanceof JsonWebTokenError) {
+      return res.status(401).json({ message: error.message });
+    }
+
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 }
